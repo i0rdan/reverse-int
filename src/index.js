557a604d3deeb1eb03f509172b1cbece08fa85bc
Evgeny Kazusev <55188582+i0rdan@users.noreply.github.com>
2021-02-07 11:18:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let x;
+  let a = '';
+  if (n < 0) x = (n * -1).toString();
+  else x = n.toString();
+  x.split('');
+  for(y = x.length-1; y >= 0; y--)
+  {
+	a += x[y];  
+  }
+  return Number(a);
 }
